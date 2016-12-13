95c30da2e4d0d46a968a4ba4386ea6eeea353d2a
Yulong Tan <yulongtan67@gmail.com>
2016-12-12 17:08:20 -0800

    load user address if logged in

--- PlanC/ProfileViewController.swift
+++ PlanC/ProfileViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 import Firebase
 
 class ProfileViewController: UIViewController {
-   
+    
     @IBOutlet weak var addressLabel: UILabel!
     @IBOutlet weak var paymentLabel: UILabel!
     @IBOutlet weak var productButton: UIButton!
@@ -30,23 +30,39 @@ class ProfileViewController: UIViewController {
         logOutButton.layer.cornerRadius = 4
         
         FIRAuth.auth()!.addStateDidChangeListener() { auth, user in
-            // 2
+            // error
             if user == nil {
                 // 3
                 self.performSegue(withIdentifier: "profileToLogInSegue", sender: self)
             } else {
                 
                 let ref = self.appDelegate.getDatabaseReference()
-                
-                
-                addressShowLabel.text = 
+                self.email = FIRAuth.auth()?.currentUser?.email
+                let newEmail = self.email.replacingOccurrences(of: ".", with: ",")
+                print("email: \(newEmail)")
+                ref.child("Users").child(newEmail).child("address").observe(.value, with: { snapshot in
+                    print("snapshot val: \(snapshot.value)")
+                    if let _ = snapshot.value as? NSDictionary {
+                        let addressRef = snapshot.value as! NSDictionary
+                        
+                        print("address accessed: \(addressRef)")
+                        let addressName = addressRef["Name"]
+                        let address = addressRef["Address"]
+                        let city = addressRef["City"]
+                        let state = addressRef["State"]
+                        let zipcode = addressRef["Zipcode"]
+                        
+                        let userAddress = Address(addressName: addressName as! String, address: address as! String, city: city as! String, state: state as! String, zipcode: zipcode as! String)
+                        self.addressShowLabel.text = userAddress.getAddressName()
+                    }
+                })
             }
         }
-
+        
         // Do any additional setup after loading the view.
         
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -71,7 +87,7 @@ class ProfileViewController: UIViewController {
         }
     }
     
-     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if (segue.identifier == "profileToAddressSegue") {
             let controller = segue.destination as! AddAddressViewController
             controller.email = email
@@ -98,15 +114,15 @@ class ProfileViewController: UIViewController {
     
     
     
-	
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
